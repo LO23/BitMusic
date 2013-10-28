c6ff46f3ff4d7d12093d205c143bbf9c754d3ae7
Abdoul Diallo <akdiallo91@gmail.com>
2013-10-28 14:01:02 +0100

    first commit

---
+++ BitMusic/bitmusic/hmi/BitMusicApplication.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hmi;
+
+/**
+ *
+ * @author khadre
+ */
+public class BitMusicApplication {
+    
+    public static void main(String[] args) {
+        MainWindow start = new MainWindow();
+        start.initialize();
+        
+    }
+}
