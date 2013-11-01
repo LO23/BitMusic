a409cab16fe4bf78b24cba2b10d2ec71113ac731
unkedeuxke <kev.lefevre@free.fr>
2013-11-01 12:38:38 +0100

    MAJ de la ConnectionView (TODO : centrer les éléments)

--- BitMusic/bitmusic/hmi/modules/connection/ConnectionView.java
+++ BitMusic/bitmusic/hmi/modules/connection/ConnectionView.java
@@ -14,6 +14,8 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import hmi.patterns.AbstractController;
 import hmi.patterns.AbstractView;
+import javax.swing.GroupLayout;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -33,34 +35,68 @@ public class ConnectionView extends AbstractView {
         
         JLabel connectionLabel = new JLabel("Connexion");
         connectionLabel.setSize(d);
-        this.panel.add(connectionLabel);
         
         JLabel loginLabel = new JLabel("Pseudo");
         loginLabel.setSize(d);
-        this.panel.add(loginLabel);
         
         JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setSize(d);  
-        this.panel.add(passwordLabel);
+        passwordLabel.setSize(d);
         
         JButton connectButton = new JButton("Se connecter");
         connectButton.setSize(d);
-        this.panel.add(connectButton);
         
         JButton resetButton = new JButton("Réinitialiser");
         resetButton.setSize(d);
-        this.panel.add(resetButton);
         
         JButton createUserButton = new JButton("Créer un compte");
         createUserButton.setSize(d);
-        this.panel.add(createUserButton);
         
         JTextField loginField = new JTextField("");
         loginField.setColumns(10);
-        this.panel.add(loginField);
         
         JPasswordField passwordField = new JPasswordField("");
         passwordField.setColumns(10);
-        this.panel.add(passwordField);
+        
+        GroupLayout layout = new GroupLayout(this.panel);
+        this.panel.setLayout(layout);
+        
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addComponent(connectionLabel)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(loginLabel)
+                    .addComponent(passwordLabel)
+                    .addComponent(connectButton)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(loginField)
+                    .addComponent(passwordField)
+                    .addComponent(resetButton)
+                )
+                .addComponent(createUserButton)
+        );
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+               .addComponent(connectionLabel)
+               .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginLabel)
+                    .addComponent(loginField)
+               )
+               .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(passwordLabel)
+                    .addComponent(passwordField)
+               )
+               .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(connectButton)
+                    .addComponent(resetButton)
+               )
+               .addComponent(createUserButton)
+        );
+        
+        layout.linkSize(SwingConstants.HORIZONTAL, loginLabel, loginField);
+        layout.linkSize(SwingConstants.HORIZONTAL, passwordLabel, passwordField);
     }
 }
