ebe59373c01f8c0af51bbbbdc278aa02319eec24
Alexis <alexis.chatillon0@gmail.com>
2013-11-01 22:42:56 +0100

    Structure de la MessageFactory To do : comment on passe les arguments de manière générique à la Factory ? Sérialisation ? Il faut encore implémenter les case, notamment pour appeler correctement les contructeurs.

--- BitMusic/bitmusic/network/message/MessageFactory.java
+++ BitMusic/bitmusic/network/message/MessageFactory.java
@@ -7,9 +7,70 @@
 package network.message;
 
 /**
- *
- * @author florian
+ * Class that implements the factory design pattern,
+ * which will provide an easy way to create
+ * different kind of messages.
+ * @author florian, alexis
  */
-public class MessageFactory {
-    
+public final class MessageFactory {
+    /**
+     * The only instance of the MessageFactory class.
+     */
+    private static MessageFactory instance = null;
+    /**
+     * The object constructor.
+     */
+    private MessageFactory() {
+        super();
+    }
+    /**
+     * The method that returns the instance of the class.
+     * @return The only instance of the MessageFactory class.
+     */
+    public static MessageFactory getInstance() {
+         if (MessageFactory.instance == null) {
+            MessageFactory.instance = new MessageFactory();
+         }
+         return MessageFactory.instance;
+     }
+    /**
+     * Function provided to easily create different kind of messages.
+     * @param type : Type of the message we want.
+     * @return The newly created message.
+     */
+    public static AbstractMessage createMessage(final EnumTypeMessage type/*,
+    passage d'un objet sérialisé contenant les arguments ?*/) {
+        switch (type) {
+            case AddComment :
+                return new MessageAddComment();
+            case ErrorNotification :
+                return new MessageErrorNotification();
+            case GetSongFile :
+                return new MessageGetSongFile();
+            case GetSongsByUser :
+                return new MessageGetSongsByUser();
+            case GetUser :
+                return new MessageGetUser();
+            case LogOut :
+                return new MessageLogOut();
+            case NotifyNewConnection :
+                return new MessageNotifyNewConnection();
+            case ReplyConnectionUser :
+                return new MessageReplyConnectionUser();
+            case SearchSongsByTag :
+                return new MessageSearchSongsByTag();
+            case SendSongList :
+                return new MessageSendSongList();
+            case SendUser :
+                return new MessageSendUser();
+            case SendSongFile :
+                return new MessageSendSongFile();
+            case TagRequest :
+                return new MessageTagRequest();
+            case UpdateCommentNotification :
+                return new MessageUpdateCommentNotification();
+            default :
+                return null;
+        }
+    }
 }
