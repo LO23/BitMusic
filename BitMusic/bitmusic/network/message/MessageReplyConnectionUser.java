b526dfbf7700099cc29f4e4c5b3eb07292fa2f9f
Alexis <alexis.chatillon0@gmail.com>
2013-11-08 14:49:04 +0100

    Développement de la classe Message ReplyConnectionUser Suppression de la Factory devenue useless Fonction treatment() surchargée est vide

--- BitMusic/bitmusic/network/message/MessageReplyConnectionUser.java
+++ BitMusic/bitmusic/network/message/MessageReplyConnectionUser.java
@@ -5,11 +5,54 @@
  */
 
 package bitmusic.network.message;
+import bitmusic.profile.User;
 
 /**
- *
+ * Message to send our profile to a newly connected user.
  * @author alexis
  */
-public class MessageReplyConnectionUser extends AbstractMessage {
+public final class MessageReplyConnectionUser extends AbstractMessage {
+    /**
+     * The profile we send to the newly connected user.
+     */
+    private User myProfile;
+
+    /**
+     * Constructor.
+     * @param paramType Type of the message
+     * @param paramIpSource IP address of the sender
+     * @param paramIpDest IP address of the receiver
+     * @param paramUser The profile we send
+     */
+    public MessageReplyConnectionUser(final EnumTypeMessage paramType,
+            final String paramIpSource, final String paramIpDest,
+            final User paramUser) {
+        super(paramType, paramIpSource, paramIpDest);
+        myProfile = paramUser;
+    }
+
+    /**
+     * .
+     */
+    @Override
+    public void treatment() {
+
+    }
+
+    /**
+     * Setter of myProfile attribute.
+     * @param paramMyProfile The profile to set
+     */
+    public void setMyProfile(final User paramMyProfile) {
+        this.myProfile = paramMyProfile;
+    }
+
+    /**
+     * Getter of myProfile attribute.
+     * @return User The profile we are about to send
+     */
+    public User getMyProfile() {
+        return myProfile;
+    }
 
 }
