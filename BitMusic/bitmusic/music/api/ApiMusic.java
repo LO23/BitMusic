8526a03b884a15913b4895ee68ad56214263e839
RymBarkaoui <rym.barkaoui@hotmail.fr>
2013-11-09 11:29:34 +0100

    [MUSIC] ApiMusic definition function

--- BitMusic/bitmusic/music/api/ApiMusic.java
+++ BitMusic/bitmusic/music/api/ApiMusic.java
@@ -5,11 +5,23 @@
  */
 
 package bitmusic.music.api;
+import bitmusic.music.data.Rights;
+import bitmusic.music.data.Comment;
+import java.util.ArrayList;
 
 /**
  *
  * @author Thomas
  */
+
 public interface ApiMusic {
+    
+    public boolean addCommentFromHmi(String songID, String commentText);
+    public boolean addCommentFromNetwork(String songID, Comment commentText);
+    public void searchSongsByUser(String userID, String searchId);   
+    public void importSong(String path, String title, String album, ArrayList<String> tags, Rights rights);
+    public void changeRigthsOfThisSong (String songid, Rights rights);
+    public void playSong (String path);
+    public String getSongFile (String songid);
     
 }
