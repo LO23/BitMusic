b7a692f93aad3231fca5467828342c8c215d59ea
Alexis <alexis.chatillon0@gmail.com>
2013-11-10 14:12:34 +0100

    Dév de la classe Message AddComment

--- BitMusic/bitmusic/network/message/MessageAddComment.java
+++ BitMusic/bitmusic/network/message/MessageAddComment.java
@@ -5,22 +5,39 @@
  */
 
 package bitmusic.network.message;
+import bitmusic.music.data.Song;
+import bitmusic.music.data.Comment;
 
 /**
  *
  * @author alexis
  */
-public class MessageAddComment extends AbstractMessage {
+public final class MessageAddComment extends AbstractMessage {
+
+    /**
+     * The song to comment.
+     */
+    private Song song;
+
+    /**
+     * The comment we want to add.
+     */
+    private Comment comment;
 
     /**
      * Constructor.
      * @param paramType Type of the message
      * @param paramIpSource IP address of the sender
      * @param paramIpDest IP address of the receiver
+     * @param paramSong The song we want to comment
+     * @param paramComment The comment we want to add
      */
     public MessageAddComment(final EnumTypeMessage paramType,
-            final String paramIpSource, final String paramIpDest) {
+            final String paramIpSource, final String paramIpDest,
+            final Song paramSong, final Comment paramComment) {
         super(paramType, paramIpSource, paramIpDest);
+        this.song = paramSong;
+        this.comment = paramComment;
     }
 
     /**
@@ -29,5 +46,37 @@ public class MessageAddComment extends AbstractMessage {
     @Override
     public void treatment() {
 
+    }
+
+    /**
+     * Setter for the song attribute.
+     * @param paramSong The song wa want to comment
+     */
+    public void setSong(final Song paramSong) {
+        this.song = paramSong;
+    }
+
+    /**
+     * Setter for the comment attribute.
+     * @param paramComment The comment we want to add
+     */
+    public void setComment(final Comment paramComment) {
+        this.comment = paramComment;
+    }
+
+    /**
+     * Getter for the song attribute.
+     * @return Song The song we want to comment
+     */
+    public Song getSong() {
+        return song;
+    }
+
+    /**
+     * Getter for the comment attribute.
+     * @return Comment The comment we want to add
+     */
+    public Comment getComment() {
+        return comment;
     }
 }
