343e5d776c2ac33acb1f21ab5a4e8ea20b7f3b10
Alexis <alexis.chatillon0@gmail.com>
2013-11-10 15:21:13 +0100

    Dév de la classe Message GetSongsByUser

--- BitMusic/bitmusic/network/message/MessageGetSongsByUser.java
+++ BitMusic/bitmusic/network/message/MessageGetSongsByUser.java
@@ -7,19 +7,42 @@
 package bitmusic.network.message;
 
 /**
- *
+ * Message send to get the song list of a distant user.
  * @author alexis
  */
-public class MessageGetSongsByUser extends AbstractMessage {
+public final class MessageGetSongsByUser extends AbstractMessage {
+    /**
+     * ID of the User that owns the list we want.
+     */
+    private String askedUser;
+
+    /**
+     * ID of the user that asks for this list.
+     */
+    private String operator;
+
+    /**
+     * ID of the research ?
+     */
+    private String researchId;
+
     /**
      * Constructor.
      * @param paramType Type of the message
      * @param paramIpSource IP address of the sender
      * @param paramIpDest IP address of the receiver
+     * @param paramAskedUser ID of the user that owns the list
+     * @param paramOperator ID of the user that asks for the list
+     * @param paramResearchId ID of the research
      */
     public MessageGetSongsByUser(final EnumTypeMessage paramType,
-            final String paramIpSource, final String paramIpDest) {
+            final String paramIpSource, final String paramIpDest,
+            final String paramAskedUser, final String paramOperator,
+            final String paramResearchId) {
         super(paramType, paramIpSource, paramIpDest);
+        askedUser = paramAskedUser;
+        operator = paramOperator;
+        researchId = paramResearchId;
     }
 
     /**
@@ -29,4 +52,54 @@ public class MessageGetSongsByUser extends AbstractMessage {
     public void treatment() {
 
     }
+
+    /**
+     * Setter of the asekedUser attribute.
+     * @param paramAskedUser ID of the user that owns the list
+     */
+    public void setAskedUser(final String paramAskedUser) {
+        this.askedUser = paramAskedUser;
+    }
+
+    /**
+     * Setter of the operator attribute.
+     * @param paramOperator ID of the user that asks for the list
+     */
+    public void setOperator(final String paramOperator) {
+        this.operator = paramOperator;
+    }
+
+    /**
+     * Setter of the researchId attribute.
+     * @param paramResearchId ID of the research
+     */
+    public void setResearchId(final String paramResearchId) {
+        this.researchId = paramResearchId;
+    }
+
+    /**
+     * Getter of the askedUser attribute.
+     * @return String Id of the user that owns the list
+     */
+    public String getAskedUser() {
+        return askedUser;
+    }
+
+    /**
+     * Getter of the operator attribute.
+     * @return String ID of the user that asks for the list
+     */
+    public String getOperator() {
+        return operator;
+    }
+
+    /**
+     * Getter of the researchId attribute.
+     * @return String ID of the research
+     */
+    public String getResearchId() {
+        return researchId;
+    }
+
+
 }
