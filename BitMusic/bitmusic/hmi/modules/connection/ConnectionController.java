4b0ef300df644ae3e8d77fbdd7c8932ede4d1c5f
unkedeuxke <kev.lefevre@free.fr>
2013-11-10 17:22:38 +0100

    Ajout de la base des ActionListeners (structure dans le ConnectionController)

--- BitMusic/bitmusic/hmi/modules/connection/ConnectionController.java
+++ BitMusic/bitmusic/hmi/modules/connection/ConnectionController.java
@@ -9,6 +9,8 @@ package bitmusic.hmi.modules.connection;
 import bitmusic.hmi.patterns.AbstractController;
 import bitmusic.hmi.patterns.AbstractModel;
 import bitmusic.hmi.patterns.AbstractView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
@@ -18,5 +20,23 @@ public final class ConnectionController extends AbstractController {
 
     public ConnectionController(final AbstractModel abstractModel, final AbstractView abstractView) {
         super(abstractModel, abstractView);
+    }
+
+    public class ConnectionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class ResetListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class CreateNewUserListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
