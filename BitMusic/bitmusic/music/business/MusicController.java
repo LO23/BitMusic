e07d5df3dda8adff0d3c4f37325a7a62b84a5d88
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2013-11-12 18:56:49 +0100

    finished the implementation of MusicController

--- BitMusic/bitmusic/music/business/MusicController.java
+++ BitMusic/bitmusic/music/business/MusicController.java
@@ -3,11 +3,31 @@
  */
 
 package bitmusic.music.business;
-
+import bitmusic.music.api.ApiMusicImpl;
 /**
  *
- * @author MB
+ * @author Amina Bouabdallah - Rym Barkaoui
  */
 public class MusicController {
+    private ApiMusicImpl apiMusic; //fichier contenant les fonctions utilisées par l'extérieur
+    private static final MusicController MUSICCONTROLLER = new MusicController();
+    
+    
+    private MusicController(){
+        apiMusic = bitmusic.music.api.ApiMusicImpl.getInstance();
+    }
+    
+    public static MusicController getInstance(){
+        return MUSICCONTROLLER;
+    }
+    
+    /*########################################################################*/
+    /* GETTERS */
+    /*########################################################################*/
+    public final ApiMusicImpl getApiMusic(){
+        return apiMusic;
+    }
+    
+    
     
 }
