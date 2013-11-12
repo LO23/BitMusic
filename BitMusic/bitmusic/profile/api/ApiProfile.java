5993968cae7ceb5a97ecc337b18e4460fe8bd712
Milio Peralta <milio.peralta@gmail.com>
2013-11-12 17:10:22 +0100

    Correction API Profile

--- BitMusic/bitmusic/profile/api/ApiProfile.java
+++ BitMusic/bitmusic/profile/api/ApiProfile.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package bitmusic.profile;
+package bitmusic.profile.api;
 
 import bitmusic.profile.classes.User;
 
