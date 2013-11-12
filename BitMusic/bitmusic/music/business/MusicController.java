0f0567f3f8c8ea24852efe6dd6b50809e3fe4d56
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2013-11-12 19:04:55 +0100

    commented the MusicController class

--- BitMusic/bitmusic/music/business/MusicController.java
+++ BitMusic/bitmusic/music/business/MusicController.java
@@ -1,33 +1,54 @@
 /*
- * Class which instantiates all the 
+ * Singleton used by classes from the HMI, Network and Profile packages, 
+ for accessing the unique ApiMusicImpl object = object containing methods implemented by Music for the outside world
  */
-
 package bitmusic.music.business;
+
 import bitmusic.music.api.ApiMusicImpl;
+
 /**
  *
  * @author Amina Bouabdallah - Rym Barkaoui
  */
 public class MusicController {
-    private ApiMusicImpl apiMusic; //fichier contenant les fonctions utilisées par l'extérieur
+
+    /**
+     * Reference to the unique ApiMusicImpl object (object containing method
+     * accessed by HMI, Network, and Profile)
+     */
+    private ApiMusicImpl apiMusic;
+
+    /**
+     * Singleton instance
+     */
     private static final MusicController MUSICCONTROLLER = new MusicController();
-    
-    
-    private MusicController(){
+
+    /**
+     * Private Constructor : creates a MusicController singleton and gets the 
+     * reference to the unique instance of ApiMusicImpl
+     */
+    private MusicController() {
         apiMusic = bitmusic.music.api.ApiMusicImpl.getInstance();
     }
-    
-    public static MusicController getInstance(){
+/**
+ * 
+ * @return the MusicController
+ */
+    public static MusicController getInstance() {
         return MUSICCONTROLLER;
     }
-    
+
     /*########################################################################*/
     /* GETTERS */
     /*########################################################################*/
-    public final ApiMusicImpl getApiMusic(){
+    
+    /**
+     * 
+     * @return the reference to the apiMusic object
+     */
+    
+    public final ApiMusicImpl getApiMusic() {
         return apiMusic;
     }
-    
-    
-    
+
 }
