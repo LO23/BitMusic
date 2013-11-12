db3e5b7d5c7724c0028e25a51ca14b02eafcbf75
Milio Peralta <milio.peralta@gmail.com>
2013-11-12 18:24:08 +0100

    [Profile] Remove Rights

--- BitMusic/bitmusic/profile/api/ApiProfile.java
+++ BitMusic/bitmusic/profile/api/ApiProfile.java
@@ -4,35 +4,105 @@
  */
 package bitmusic.profile.api;
 
+import bitmusic.profile.classes.Category;
 import bitmusic.profile.classes.User;
-
-import bitmusic.music.data.Song;
 import java.util.ArrayList;
-
+import java.util.Date;
 /**
  *
  * @author saben2
  */
-public class Profile {
-    //########################## ATTRIBUTES ##########################//
-
-    private static User currentUser;
-
-    //########################### METHODS ##############################//
+public interface ApiProfile {
     /*
     public boolean checkPassword(String login, String password){
         return true;
     }
-    public void saveUser(User user){
-    }*/
+    */
 
-    public static User getCurrentUser() {
-        return Profile.currentUser;
-    }
+    /**
+     * Creates a User as an object User
+     *
+     * @return
+     */
+    public void createUser(String login, String password, String firstName, String lastName, Date birthDate, String avatarPath);
 
-    public static void setCurrentUser(User newUser) {
-        Profile.currentUser = newUser;
-    }
+    /**
+     * Saves a User as an object User
+     *
+     * @return
+     */
+    public void saveUser(User user);
+
+    /**
+     * Returns the current user as an object User
+     *
+     * @return User user
+     */
+    public User getCurrentUser();
+
+    /**
+     * Returns the folder of the current user
+     *
+     * @return String folderName
+     */
+    public String getCurrentUserFolder();
+
+    /**
+     * Returns the categories names of an user
+     *
+     * @return ArrayList<String>
+     */
+    public ArrayList<String> getCategoriesNameByUserId();
+
+    /**
+     * Returns a category name of an user
+     *
+     * @return ArrayList<String>
+     */
+    public ArrayList<Category> getCategories();
+
+    /**
+     * Adds a category to the categories of an user
+     *
+     * @param name
+     * @return
+     */
+    public void addCategory(String name);
+
+    /**
+     * Updates a category of an user
+     *
+     * @param categoryId
+     * @param newName
+     * @return
+     */
+    public void updateCategory(int categoryId, String newName);
+
+    /**
+     * Deletes a category of an user
+     *
+     * @return
+     */
+    public void deleteCategory(int categoryId);
+
+    /**
+     * Adds a user to a category
+     *
+     * @param user
+     * @param categoryId
+     * @return
+     */
+    public void addUserToCategory(User user, int categoryId);
+
+    /**
+     * Moves a user to another category
+     *
+     * @param userId
+     * @param categoryId
+     * @return
+     */
+    public void moveContact(String userId, int categoryId);
+
 
     /*public User getCurrentUser(String userId){
         if(currentUser.getUserId()==userId){
