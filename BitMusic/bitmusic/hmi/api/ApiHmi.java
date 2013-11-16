6a9be732cb65aa430be881e031f82553dacfef0c
unkedeuxke <kev.lefevre@free.fr>
2013-11-16 09:40:45 +0100

    Ajout de la JavaDoc dans l'interface IHM pour quelques méthodes ET préparation de nouvelles méthodes à implémenter l'API (les autres modules peuvent utiliser ces méthodes)

--- BitMusic/bitmusic/hmi/api/ApiHmi.java
+++ BitMusic/bitmusic/hmi/api/ApiHmi.java
@@ -6,17 +6,80 @@
 
 package bitmusic.hmi.api;
 
+import bitmusic.music.business.SongPlayer;
+import bitmusic.music.data.Rights;
+import bitmusic.music.data.Song;
 import bitmusic.music.data.SongLibrary;
+import bitmusic.profile.classes.Category;
 import bitmusic.profile.classes.User;
+import java.util.ArrayList;
 
 /**
  *
  * @author hebergui <hebergui.utc@gmail.com>
  */
 public interface ApiHmi {
-    public abstract void notifyNewConnection(User lightUserLan);
-    public abstract void removeUserFromOnlineUsers(String userId);
-    public abstract void notifyLightProfile(User user, String searchId);
-    public abstract void notifySongListByUserId(String userID, SongLibrary songLibrary);
-    public abstract void notifySongListBySearchId(String searchId, SongLibrary songList);
+
+    /**
+    * Notify a new user connection to HMI
+    *
+    * @param lightUserLan   The light user profile
+    * @return void
+    */
+    void notifyNewConnection(User lightUserLan);
+
+    /**
+    * Remove user from the online-users-frame of others users still online
+    *
+    * @param userId
+    * @return void
+    */
+    void removeUserFromOnlineUsers(String userId);
+
+    /**
+    * Receive a user profile
+    *
+    * @param user       The user profile
+    * @param searchId   String of the search ID
+    * @return void
+    */
+    void notifyLightProfile(User user, String searchId);
+
+    /**
+    * Receive the songlist from a user
+    * Print all the music of one user
+    *
+    * @param userId	String of the user ID
+    * @param songList	Song library containing songs
+    * @return void
+    */
+    void notifySongListByUserId(String userId, SongLibrary songList);
+
+    /**
+    * Notify the HMI about the reception of a song list
+    *
+    * @param searchId	Research ID used by the HMI
+    * @param songList	Song library containing songs
+    * @return void
+    */
+    void notifySongListBySearchId(String searchId, SongLibrary songList);
+
+    // TODO : vérifier leur utilité pour les autres modules
+    void displayCategories();
+    //void addCategory(String name, boolean play, readInfos, boolean comment, boolean rate);
+    void removeCategory();
+    void searchSongsByUser(String userId);
+    void displayUser(String userID);
+    void importSong(String path, String title, String artist, String album, ArrayList<String> tags, Rights rights);
+    boolean addComment(Song song, String comment);
+    void addUserToCategory(User user, Category category);
+    void logOut();
+    void closeApp();
+    void playRemoteSong();
+    void removeUserFromCategory(User user, Category category);
+    void manageRightsForOneSong(String songId, Rights rights);
+    void startSong(SongPlayer player);
+    void connectUser(String login, String mdp);
+    void displaySongs(String userId);
+    void errorNotification(String errorMessage);
 }
