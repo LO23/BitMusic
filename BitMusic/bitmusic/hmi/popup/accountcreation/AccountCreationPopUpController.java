f562e13759c9f380a1a89c90480efec0a39c3ac0
delbouki <elboukili.elhassani.driss@gmail.com>
2013-11-19 19:32:47 +0100

    Focus sur la date

--- BitMusic/bitmusic/hmi/popup/accountcreation/AccountCreationPopUpController.java
+++ BitMusic/bitmusic/hmi/popup/accountcreation/AccountCreationPopUpController.java
@@ -10,8 +10,11 @@ import bitmusic.hmi.mainwindow.WindowComponent;
 import bitmusic.hmi.patterns.AbstractController;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -34,6 +37,42 @@ public final class AccountCreationPopUpController extends AbstractController<Acc
                 path.setText(file.getSelectedFile().getPath());
             }
         }
+    }
+
+    /**
+     *
+     */
+    public class HintTextFieldListener extends JTextField implements FocusListener {
+
+        private final String hint = "dd/MM/yyyy";
+        private boolean showingHint;
+
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            System.out.println("Focus Gained");
+            if(this.getText().isEmpty())
+            {
+                this.setText("");
+                super.setText("");
+                showingHint = false;
+            }
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            System.out.println("Focus Lost");
+            if(this.getText().isEmpty())
+            {
+                this.setText(hint);
+                super.setText(hint);
+                showingHint = true;
+            }
+        }
+        @Override
+        public String getText() {
+            return showingHint ? "" : super.getText();
+            }
     }
 
     public class CancelListener implements ActionListener {
