9232ec5b3d60d9b9e877dfdd907d826441ccb9f0
hebergui <hebergui.utc@gmail.com>
2013-11-24 16:45:59 +0100

    Modification de la vue de la poup MyProfile

--- BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpView.java
+++ BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpView.java
@@ -26,16 +26,17 @@ import javax.swing.SwingConstants;
  */
 public final class ModifyProfilePopUpView extends AbstractView<ModifyProfilePopUpController> {
 
-    private final JLabel myProfileLabel = new JLabel("Mon profil");
+    private final JLabel myProfileLabel = new JLabel("<html><u> Présentation de mon profil</u></html>");
     private final JLabel prenomLabel = new JLabel("Prénom ");
-    private final JTextField prenomField = new JTextField("");
+    private JTextField prenomField = new JTextField("");
     private final JLabel nomLabel = new JLabel("Nom ");
-    private final JTextField nomField = new JTextField("");
+    private JTextField nomField = new JTextField("");
     private final JLabel birthLabel = new JLabel("Date de Naissance (*)");
     private final DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-    private final JFormattedTextField dateTextField = new JFormattedTextField(format);
+    private JFormattedTextField dateTextField = new JFormattedTextField(format);
     private final JLabel avatarLabel = new JLabel("Avatar");
-    private final JButton avatarbrowseButton = new JButton("Parcourir...");
+    private final JButton avatarBrowseButton = new JButton("Parcourir...");
+    private JTextField avatarField = new JTextField("");
     private final JButton submitButton = new JButton("Soumettre");
     private final JButton cancelButton = new JButton("Annuler");
 
@@ -49,94 +50,72 @@ public final class ModifyProfilePopUpView extends AbstractView<ModifyProfilePopU
     public void initPanel() {
         System.out.println("--- ModifyProfilePopUpView.initPanel()");
 
-       final Dimension d = new Dimension(80, 20);
-
-
-        this.myProfileLabel.setSize(d);
-
-        this.prenomLabel.setSize(d);
-        this.prenomField.setColumns(10);
-        this.nomLabel.setSize(d);
-        this.nomField.setColumns(10);
-        this.birthLabel.setSize(d);
-        this.dateTextField.setColumns(10);
         this.dateTextField.setText("dd/MM/yyyy");
-        this.avatarLabel.setSize(d);
-        this.avatarbrowseButton.setSize(d);
 
-        this.submitButton.setSize(d);
-        this.cancelButton.setSize(d);
-
-
-
-        avatarbrowseButton.addActionListener(this.getController().new AvatarBrowseListener());
-
+        this.avatarBrowseButton.addActionListener(this.getController().new AvatarBrowseListener());
         this.cancelButton.addActionListener(this.getController().new CancelListener());
-
         this.dateTextField.addFocusListener(this.getController().new HintTextFieldListener());
+
 
         GroupLayout layout = new GroupLayout(this.getPanel());
         this.getPanel().setLayout(layout);
 
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
-
-        layout.setHorizontalGroup(
-            layout.createSequentialGroup()
-                .addComponent(myProfileLabel)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(prenomLabel)
-                    .addComponent(nomLabel)
-                    .addComponent(birthLabel)
-                    .addComponent(avatarLabel)
-                    .addComponent(submitButton)
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(prenomField)
-                    .addComponent(nomField)
-                    .addComponent(dateTextField)
-                    .addComponent(avatarbrowseButton)
-
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(cancelButton)
-                )
-
+                layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(myProfileLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(birthLabel)
+                                    .addComponent(avatarLabel)
+                                    .addComponent(nomLabel)
+                                    .addComponent(prenomLabel))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(dateTextField)
+                                    .addComponent(avatarField)
+                                    .addComponent(nomField, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                                    .addComponent(prenomField)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(cancelButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                                .addComponent(submitButton)))
+                        .addGap(18, 18, 18)
+                        .addComponent(avatarBrowseButton)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-            layout.createSequentialGroup()
-                .addComponent(myProfileLabel)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(myProfileLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(prenomLabel)
-                    .addComponent(prenomField)
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(prenomField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(nomLabel)
-                    .addComponent(nomField)
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(nomField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(birthLabel)
-                    .addComponent(dateTextField)
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(dateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(avatarLabel)
-                    .addComponent(avatarbrowseButton)
-                )
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(submitButton))
-
+                    .addComponent(avatarField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(avatarBrowseButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cancelButton)
-                )
+                    .addComponent(submitButton))
+                .addContainerGap())
         );
-
-
-        layout.linkSize(SwingConstants.HORIZONTAL, prenomLabel, prenomField);
-        layout.linkSize(SwingConstants.HORIZONTAL, nomLabel, nomField);
-        layout.linkSize(SwingConstants.HORIZONTAL, birthLabel, dateTextField);
-        layout.linkSize(SwingConstants.HORIZONTAL, avatarLabel, avatarbrowseButton);
-        layout.linkSize(SwingConstants.HORIZONTAL, submitButton, cancelButton);
-
 
     }
 
