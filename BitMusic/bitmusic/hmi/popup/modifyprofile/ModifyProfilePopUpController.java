f060e3d217a09fc4287d5831206ce197d300954d
stephysim <stephanie.simeu@gmail.com>
2013-11-24 11:40:58 +0100

    Vue de Mon Profil

--- BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpController.java
+++ BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpController.java
@@ -10,6 +10,11 @@ import bitmusic.hmi.mainwindow.WindowComponent;
 import bitmusic.hmi.patterns.AbstractController;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JTextField;
 
 /**
  *
@@ -21,12 +26,68 @@ public final class ModifyProfilePopUpController extends AbstractController<Modif
         super(model, view);
     }
 
-    public class CancelListener implements ActionListener {
+     public class AvatarBrowseListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("---- Clic sur le bouton Parcourir");
+            JFileChooser file = new JFileChooser();
+            JLabel path = new JLabel();
+            int returnVal = file.showSaveDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                System.out.println("---- OK");
+                path.setText(file.getSelectedFile().getPath());
+            }
+        }
+    }
+
+      public class HintTextFieldListener extends JTextField implements FocusListener {
+
+        private final String hint = "dd/MM/yyyy";
+        private boolean showingHint;
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            System.out.println("Focus Gained");
+            if(this.getText().isEmpty())
+            {
+                this.setText("");
+                super.setText("");
+                showingHint = false;
+            }
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            System.out.println("Focus Lost");
+            if(this.getText().isEmpty())
+            {
+                this.setText(hint);
+                super.setText(hint);
+                showingHint = true;
+            }
+        }
+        @Override
+        public String getText() {
+            return showingHint ? "" : super.getText();
+            }
+    }
+
+      public class CancelListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("---- Clic sur le bouton Annuler");
+            WindowComponent.getInstance().getMyProfileComponent().getController().getPopUp().dispose();
+          }
+       }
+
+    //public class CancelListener implements ActionListener {
+       // @Override
+       // public void actionPerformed(ActionEvent e) {
+           // System.out.println("---- Clic sur le bouton Annuler");
             // À décommenter dès que la PopUp est implémentée dans le XXXXXXXXComponent (créant la PopUp)
             //WindowComponent.getInstance().getXXXXXXXXComponent().getController().getPopUp().dispose();
-        }
-    }
+        //}
+    //}
+
+
 }
--- BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpView.java
+++ BitMusic/bitmusic/hmi/popup/modifyprofile/ModifyProfilePopUpView.java
@@ -8,12 +8,36 @@ package bitmusic.hmi.popup.modifyprofile;
 
 import bitmusic.hmi.patterns.AbstractView;
 import bitmusic.hmi.patterns.Observable;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 /**
  *
  * @author unkedeuxke
  */
 public final class ModifyProfilePopUpView extends AbstractView<ModifyProfilePopUpController> {
+
+    private final JLabel myProfileLabel = new JLabel("Mon profil");
+    private final JLabel prenomLabel = new JLabel("Prénom ");
+    private final JTextField prenomField = new JTextField("");
+    private final JLabel nomLabel = new JLabel("Nom ");
+    private final JTextField nomField = new JTextField("");
+    private final JLabel birthLabel = new JLabel("Date de Naissance (*)");
+    private final DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    private final JFormattedTextField dateTextField = new JFormattedTextField(format);
+    private final JLabel avatarLabel = new JLabel("Avatar");
+    private final JButton avatarbrowseButton = new JButton("Parcourir...");
+    private final JButton submitButton = new JButton("Soumettre");
+    private final JButton cancelButton = new JButton("Annuler");
 
     private final String type = "POPUP";
 
@@ -25,8 +49,97 @@ public final class ModifyProfilePopUpView extends AbstractView<ModifyProfilePopU
     public void initPanel() {
         System.out.println("--- ModifyProfilePopUpView.initPanel()");
 
-        // TODO
+       final Dimension d = new Dimension(80, 20);
+
+
+        this.myProfileLabel.setSize(d);
+
+        this.prenomLabel.setSize(d);
+        this.prenomField.setColumns(10);
+        this.nomLabel.setSize(d);
+        this.nomField.setColumns(10);
+        this.birthLabel.setSize(d);
+        this.dateTextField.setColumns(10);
+        this.dateTextField.setText("dd/MM/yyyy");
+        this.avatarLabel.setSize(d);
+        this.avatarbrowseButton.setSize(d);
+
+        this.submitButton.setSize(d);
+        this.cancelButton.setSize(d);
+
+
+
+        avatarbrowseButton.addActionListener(this.getController().new AvatarBrowseListener());
+
+        this.cancelButton.addActionListener(this.getController().new CancelListener());
+
+        this.dateTextField.addFocusListener(this.getController().new HintTextFieldListener());
+
+        GroupLayout layout = new GroupLayout(this.getPanel());
+        this.getPanel().setLayout(layout);
+
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addComponent(myProfileLabel)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(prenomLabel)
+                    .addComponent(nomLabel)
+                    .addComponent(birthLabel)
+                    .addComponent(avatarLabel)
+                    .addComponent(submitButton)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(prenomField)
+                    .addComponent(nomField)
+                    .addComponent(dateTextField)
+                    .addComponent(avatarbrowseButton)
+
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(cancelButton)
+                )
+
+        );
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(myProfileLabel)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(prenomLabel)
+                    .addComponent(prenomField)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(nomLabel)
+                    .addComponent(nomField)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(birthLabel)
+                    .addComponent(dateTextField)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(avatarLabel)
+                    .addComponent(avatarbrowseButton)
+                )
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(submitButton))
+
+                    .addComponent(cancelButton)
+                )
+        );
+
+
+        layout.linkSize(SwingConstants.HORIZONTAL, prenomLabel, prenomField);
+        layout.linkSize(SwingConstants.HORIZONTAL, nomLabel, nomField);
+        layout.linkSize(SwingConstants.HORIZONTAL, birthLabel, dateTextField);
+        layout.linkSize(SwingConstants.HORIZONTAL, avatarLabel, avatarbrowseButton);
+        layout.linkSize(SwingConstants.HORIZONTAL, submitButton, cancelButton);
+
+
     }
+
 
     @Override
     public String getType() {
@@ -35,6 +148,9 @@ public final class ModifyProfilePopUpView extends AbstractView<ModifyProfilePopU
 
     @Override
     public void update(Observable obj, String str) {
-        System.out.println("----- ModifyProfilePopUpView.update()");
+        System.out.println("----- ModifyProfilePopUpView.update() ->" + str);
+
+
     }
+
 }
