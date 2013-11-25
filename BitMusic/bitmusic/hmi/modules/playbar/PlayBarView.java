7cfb824631a2044ce322beba163048acde4d6663
Abdoul Diallo <akdiallo91@gmail.com>
2013-11-26 00:21:19 +0100

    PlayBarComponent: Icons added

--- BitMusic/bitmusic/hmi/modules/playbar/PlayBarView.java
+++ BitMusic/bitmusic/hmi/modules/playbar/PlayBarView.java
@@ -10,7 +10,9 @@ import bitmusic.hmi.patterns.AbstractView;
 import bitmusic.hmi.patterns.Observable;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
@@ -22,18 +24,34 @@ public final class PlayBarView extends AbstractView<PlayBarController> {
 
     private final String type = "SOUTH";
     // Ces boutons deviendront plutard des images cliquables
-    private final JButton playButton = new JButton("Play");
-    private final JButton stopButton = new JButton("Stop");
-    private final JButton downloadButton = new JButton("Download");
+    private ImageIcon playIcon ;
+    private JButton playButton;
+
+    private ImageIcon stopIcon ;
+    private JButton stopButton ;
+
+    private ImageIcon downloadIcon ;
+    private JButton downloadButton ;
     // TODO playing bar
 
     public PlayBarView() {
         super();
+
+        playIcon = new ImageIcon(this.getClass().getResource("/bitmusic/hmi/modules/playbar/icons/playicon.png"));
+        playButton = new JButton(playIcon);
+
+        stopIcon = new ImageIcon(this.getClass().getResource("/bitmusic/hmi/modules/playbar/icons/stopicon.png"));
+        stopButton = new JButton(stopIcon);
+
+        downloadIcon = new ImageIcon(this.getClass().getResource("/bitmusic/hmi/modules/playbar/icons/downloadicon.png"));
+        downloadButton = new JButton(downloadIcon);
     }
 
     @Override
     public void initPanel() {
         System.out.println("--- PlayBarView.initPanel()");
+
+
 
         final Dimension d = new Dimension(40, 10);
 
@@ -41,15 +59,19 @@ public final class PlayBarView extends AbstractView<PlayBarController> {
         this.stopButton.setSize(d);
         this.downloadButton.setSize(d);
 
+        /*playButton.setBorder(BorderFactory.createEmptyBorder());
+        stopButton.setBorder(BorderFactory.createEmptyBorder());
+        downloadButton.setBorder(BorderFactory.createEmptyBorder());*/
+        downloadButton.setContentAreaFilled(false);
+        playButton.setContentAreaFilled(false);
+        stopButton.setContentAreaFilled(false);
+
+
         this.playButton.addActionListener(getController().new PlayListener());
         this.stopButton.addActionListener(getController().new StopListener());
         this.downloadButton.addActionListener(getController().new DownloadListener());
 
-        /*GridLayout layout = new GridLayout(0,2);
-        this.getPanel().setLayout(layout);
-        this.getPanel().add(this.stopButton);
-        this.getPanel().add(this.playButton);
-        this.getPanel().add(this.downloadButton);*/
+
         GroupLayout layout = new GroupLayout(this.getPanel());
         this.getPanel().setLayout(layout);
 
@@ -94,8 +116,35 @@ public final class PlayBarView extends AbstractView<PlayBarController> {
         return downloadButton;
     }
 
+    public ImageIcon getPlayIcon() {
+        return playIcon;
+    }
+
+    public void setPlayIcon(ImageIcon playIcon) {
+        this.playIcon = playIcon;
+        this.playButton.setIcon(this.playIcon);
+    }
+
+    public ImageIcon getStopIcon() {
+        return stopIcon;
+    }
+
+    public void setStopIcon(ImageIcon stopIcon) {
+        this.stopIcon = stopIcon;
+    }
+
+    public ImageIcon getDownloadIcon() {
+        return downloadIcon;
+    }
+
+    public void setDownloadIcon(ImageIcon downloadIcon) {
+        this.downloadIcon = downloadIcon;
+    }
+
     @Override
     public void update(Observable obj, String str) {
         System.out.println("----- PlayBarView.update()");
     }
+
+
 }
