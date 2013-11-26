ba2632c7662bfa3fa4f05d64290746a2216a4c25
unkedeuxke <kev.lefevre@free.fr>
2013-11-26 12:03:47 +0100

    Ajout des listeners et des boutons dans le TabComponent

--- BitMusic/bitmusic/hmi/modules/tab/TabController.java
+++ BitMusic/bitmusic/hmi/modules/tab/TabController.java
@@ -8,9 +8,13 @@ package bitmusic.hmi.modules.tab;
 
 import bitmusic.hmi.mainwindow.WindowComponent;
 import bitmusic.hmi.patterns.AbstractController;
+import bitmusic.music.data.Song;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 /**
  *
@@ -29,5 +33,57 @@ public final class TabController extends AbstractController<TabModel, TabView> {
             String tabTitle = TabController.this.getView().getTitle();
             tabbedPane.removeTabAt(tabbedPane.indexOfTab(tabTitle));
         }
+    }
+
+    private Action editer = new AbstractAction() {
+        public void actionPerformed(ActionEvent e) {
+            JTable table = (JTable)e.getSource();
+            int row = Integer.valueOf( e.getActionCommand() );
+            Song song = ((TabModel.TabTableModel)table.getModel()).getSongAt(row);
+            System.out.println("---- Clic sur Éditer de la Song : " + song.getSongId());
+        }
+    };
+
+    private Action infos = new AbstractAction() {
+        public void actionPerformed(ActionEvent e) {
+            JTable table = (JTable)e.getSource();
+            int row = Integer.valueOf( e.getActionCommand() );
+            Song song = ((TabModel.TabTableModel)table.getModel()).getSongAt(row);
+            System.out.println("---- Clic sur Infos de la Song : " + song.getSongId());
+        }
+    };
+
+    private Action noter = new AbstractAction() {
+        public void actionPerformed(ActionEvent e) {
+            JTable table = (JTable)e.getSource();
+            int row = Integer.valueOf( e.getActionCommand() );
+            Song song = ((TabModel.TabTableModel)table.getModel()).getSongAt(row);
+            System.out.println("---- Clic sur Noter de la Song : " + song.getSongId());
+        }
+    };
+
+    private Action sauvegarder = new AbstractAction() {
+        public void actionPerformed(ActionEvent e) {
+            JTable table = (JTable)e.getSource();
+            int row = Integer.valueOf( e.getActionCommand() );
+            Song song = ((TabModel.TabTableModel)table.getModel()).getSongAt(row);
+            System.out.println("---- Clic sur Sauvegarder de la Song : " + song.getSongId());
+        }
+    };
+
+    public Action getEditer() {
+        return this.editer;
+    }
+
+    public Action getInfos() {
+        return this.infos;
+    }
+
+    public Action getNoter() {
+        return this.noter;
+    }
+
+    public Action getSauvegarder() {
+        return this.sauvegarder;
     }
 }
