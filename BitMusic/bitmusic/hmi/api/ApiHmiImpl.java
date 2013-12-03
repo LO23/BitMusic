17d8f19896750f6f7984ecd60fe5e07eb8aa9111
hebergui <hebergui.utc@gmail.com>
2013-12-03 22:08:57 +0100

    Correction post intégration

--- BitMusic/bitmusic/hmi/api/ApiHmiImpl.java
+++ BitMusic/bitmusic/hmi/api/ApiHmiImpl.java
@@ -14,6 +14,7 @@ import bitmusic.music.data.SongLibrary;
 import bitmusic.profile.classes.Category;
 import bitmusic.profile.classes.User;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -138,7 +139,11 @@ public final class ApiHmiImpl implements ApiHmi {
     }
 
     @Override
-    public void errorNotification(final String errorMessage) {
-        // TODO
+    public void errorNotification(final String moduleName, final String errorMessage) {
+        JOptionPane.showMessageDialog(
+                         null,
+                         errorMessage,
+                         "Erreur de " + moduleName,
+                         JOptionPane.ERROR_MESSAGE);
     }
 }
