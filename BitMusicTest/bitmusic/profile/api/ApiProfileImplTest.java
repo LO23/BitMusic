7042d1ded3dc04731c02ffc1a849fb0e5ee13ce7
Abdoul Diallo <akdiallo91@gmail.com>
2013-12-09 11:58:42 +0100

    Re added the BitMusicTest folder of the PR #372

---
+++ BitMusicTest/bitmusic/profile/api/ApiProfileImplTest.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bitmusic.profile.api;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Calendar;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import bitmusic.profile.classes.User;
+import bitmusic.profile.utilities.ProfileExceptions;
+
+/**
+ *
+ * @author Milio, Jérémy
+ */
+public class ApiProfileImplTest {
+	private static final String   LOGIN      = "logintest";
+	private static final String   PASSWORD   = "passwordtest";
+	private static final String   FIRSTNAME  = "firstnametest";
+	private static final String   LASTNAME   = "lastnametest";
+	private static final String   AVATARPATH = "avatarpathtest";
+	private Calendar birthdate  = Calendar.getInstance();
+    private User userTest = null;
+    private ApiProfile api = null;
+
+    public ApiProfileImplTest() {
+    	api = ApiProfileImpl.getApiProfile();
+		birthdate.set(2013, 11, 15);
+		userTest = new User(LOGIN, PASSWORD, FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test
+    public void testCreateUser() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, PASSWORD, FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+        assertEquals(userTest, api.getCurrentUser());
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithEmptyLogin() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser("", PASSWORD, FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+        fail("testCreateUserWithEmptyLogin failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithNullLogin() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(null, PASSWORD, FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+        fail("testCreateUserWithNullLogin failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithEmptyPassword() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, "", FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+        fail("testCreateUserWithEmptyPassword failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithNullPassword() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, null, FIRSTNAME, LASTNAME, birthdate, AVATARPATH);
+        fail("testCreateUserWithNullPassword failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithNullBirthdate() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, PASSWORD, FIRSTNAME, LASTNAME, null, AVATARPATH);
+        fail("testCreateUserWithNullBirthdate failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithEmptyAvatarPath() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, PASSWORD, FIRSTNAME, LASTNAME, birthdate, "");
+        fail("testCreateUserWithEmptyAvatarPath failed");
+    }
+    
+    /**
+     * Test of createUser method, of class ApiProfileImpl.
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCreateUserWithNullAvatarPath() throws ProfileExceptions {
+        System.out.println("createUser");
+        api.createUser(LOGIN, PASSWORD, FIRSTNAME, LASTNAME, birthdate, null);
+        fail("testCreateUserWithNullAvatarPath failed");
+    }
+
+    /**
+     * Test of checkPassword method, of class ApiProfileImpl.
+     * @throws ProfileExceptions 
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCheckPasswordWithEmptyLogin() throws ProfileExceptions {
+    	System.out.println("checkPassword");
+    	api.checkPassword("", PASSWORD);
+    	fail("testCheckPasswordWithEmptyLogin failed");
+    }
+
+    /**
+     * Test of checkPassword method, of class ApiProfileImpl.
+     * @throws ProfileExceptions 
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCheckPasswordWithNullLogin() throws ProfileExceptions {
+    	System.out.println("checkPassword");
+    	api.checkPassword(null, PASSWORD);
+    	fail("testCheckPasswordWithNullLogin failed");
+    }
+
+    /**
+     * Test of checkPassword method, of class ApiProfileImpl.
+     * @throws ProfileExceptions 
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCheckPasswordWithEmptyPassword() throws ProfileExceptions {
+    	System.out.println("checkPassword");
+    	api.checkPassword(LOGIN, "");
+    	fail("testCheckPasswordWithEmptyPassword failed");
+    }
+
+    /**
+     * Test of checkPassword method, of class ApiProfileImpl.
+     * @throws ProfileExceptions 
+     */
+    @Test(expected=ProfileExceptions.class)
+    public void testCheckPasswordWithNullPassword() throws ProfileExceptions {
+    	System.out.println("checkPassword");
+    	api.checkPassword(LOGIN, null);
+    	fail("testCheckPasswordWithNullPassword failed");
+    }
+
+}
---
+++ BitMusicTest/bitmusic/profile/classes/CategoryTest.java
@@ -0,0 +1,193 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bitmusic.profile.classes;
+
+import bitmusic.music.data.Rights;
+import bitmusic.profile.utilities.ProfileExceptions;
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author frogerfa, Nico
+ */
+public class CategoryTest {
+    Category cat = new Category("famille");
+
+    public CategoryTest() {
+        Category cat = new Category("famille");
+    }
+
+    @Before
+    public void setUp() {
+        //System.out.println("Before : " + cat.getName());
+    }
+
+    @After
+    public void tearDown() {
+        //System.out.println("After : " + cat.getName());
+    }
+/**
+     * Test of setName method, of class Category.
+     */
+    @Test
+    public void testSetName() throws ProfileExceptions{
+        System.out.println("setName");
+        String newName = "newName";
+        cat.setName(newName);
+        assertEquals(newName, cat.getName());
+    }
+
+    /**
+     * @throws ProfileExceptions
+     */
+
+    @Test(expected = ProfileExceptions.class)
+    public void testSetNameNullName() throws ProfileExceptions {
+        System.out.println("setName");
+        String name = null;
+        cat.setName(name);
+    }
+
+    /**
+     * @throws ProfileExceptions
+     */
+
+    @Test(expected = ProfileExceptions.class)
+    public void testSetNameEmptyName() throws ProfileExceptions {
+        System.out.println("setName");
+        String name = "";
+        cat.setName(name);
+    }
+
+    /**
+     * Test of getName method, of class Category.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        assertEquals("famille", cat.getName());
+    }
+
+    /**
+     * Test of getContacts method, of class Category.
+     */
+    @Test
+    public void testGetContacts()  throws ProfileExceptions{
+        System.out.println("getContacts");
+        User test1 = new User("login1","mdp1");
+        User test2 = new User("login2","mdp2");
+
+        ArrayList<User> expResult = new ArrayList<User>();
+        expResult.add(test1);
+        expResult.add(test2);
+
+        cat.addUser(test1);
+        cat.addUser(test2);
+
+        assertEquals(expResult, cat.getContacts());
+    }
+
+    /**
+     * Test of setContacts method, of class Category.
+     */
+    @Test
+    public void testSetContacts() {
+        System.out.println("setContacts");
+        User test1 = new User("login1","mdp1");
+        User test2 = new User("login2","mdp2");
+
+        ArrayList<User> expResult = new ArrayList<User>();
+        expResult.add(test1);
+        expResult.add(test2);
+
+        cat.setContacts(expResult);
+
+        assertEquals(expResult, cat.getContacts());
+    }
+
+    /**
+     * Test of getRight method, of class Category.
+     */
+    @Test
+    public void testGetRight() {
+        System.out.println("getRight");
+
+        Rights expResult = new Rights(true, true, true, true);
+        expResult.equals(cat.getRight());
+    }
+
+    /**
+     * Test of updateRight method, of class Category.
+     */
+    @Test
+    public void testUpdateRight() {
+        System.out.println("updateRight");
+        Rights test = new Rights(true, false, false, false);
+        cat.updateRight(true, false, false, false);
+        test.equals(cat.getRight());
+    }
+
+    /**
+     * Test of addUser method, of class Category.
+     */
+    @Test
+    public void testAddUser()  throws ProfileExceptions{
+        List<User> liste = new ArrayList<User> (cat.getContacts());
+        User test = new User("login", "mdp");
+        cat.addUser(test);
+        liste.add(test);
+        assertEquals(liste,cat.getContacts());
+    }
+
+    /**
+     * Test of deleteUser method, of class Category.
+     */
+    @Test
+    public void testDeleteUser() throws ProfileExceptions {
+        System.out.println("deleteUser");
+
+        User test1 = new User("login1","mdp1");
+        User test2 = new User("login2","mdp2");
+
+        ArrayList<User> expResult = new ArrayList<User>();
+        expResult.add(test1);
+
+        cat.setContacts(expResult);
+        cat.addUser(test2);
+        cat.deleteUser(test2);
+
+        assertEquals(expResult, cat.getContacts());
+    }
+
+    /**
+     * Test of findContact method, of class Category.
+     */
+    @Test
+    public void testFindContact() throws ProfileExceptions {
+        System.out.println("findContact");
+        String UserID = "test1";
+
+        User test1 = new User("login1","mdp1");
+        User test2 = new User("login2","mdp2");
+
+        ArrayList<User> expResult = new ArrayList<User>();
+        expResult.add(test1);
+        expResult.add(test2);
+
+        cat.setContacts(expResult);
+
+        assertEquals(test1, cat.findContact(test1.getUserId()));
+    }
+
+}
