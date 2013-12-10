a5406a2f5a9ebc518bd8840c30c2098c31958e3e
Germain <tanguy.gum@gmail.com>
2013-12-10 19:24:12 +0100

    [IHM] finish rate song

--- BitMusic/bitmusic/hmi/popup/ratesong/RateSongPopUpController.java
+++ BitMusic/bitmusic/hmi/popup/ratesong/RateSongPopUpController.java
@@ -9,6 +9,7 @@ package bitmusic.hmi.popup.ratesong;
 import bitmusic.hmi.mainwindow.WindowComponent;
 import bitmusic.hmi.patterns.AbstractController;
 import bitmusic.hmi.popup.editsong.EditSongPopUpController;
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -43,11 +44,29 @@ public final class RateSongPopUpController extends AbstractController<RateSongPo
      public class ValiderListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("---- Clic sur le bouton Annuler");
-//            if (RateSongPopUpController.this.getView().getSongRater0()) {
-//
-//            }
+            System.out.println("---- Clic sur le bouton valider");
+
+            Integer grade = 0;
+
+            if (RateSongPopUpController.this.getView().getSongRater1().isSelected()) {
+                grade = 1;
+            }
+            else if (RateSongPopUpController.this.getView().getSongRater2().isSelected()) {
+                grade = 2;
+            }
+            else if (RateSongPopUpController.this.getView().getSongRater3().isSelected()) {
+                grade = 3;
+            }
+            else if (RateSongPopUpController.this.getView().getSongRater4().isSelected()) {
+                grade = 4;
+            }
+            else if (RateSongPopUpController.this.getView().getSongRater5().isSelected()) {
+                grade =5;
+            }
+
+
             WindowComponent win = WindowComponent.getInstance();
+            win.getApiMusic().addGradeFromHmi(RateSongPopUpController.this.getModel().getSong().getSongId(), grade);
             int parentTabId = RateSongPopUpController.this.getView().getParentTabId();
             win.getCentralAreaComponent().getView().getTabComponent(parentTabId).getController().getPopUp().dispose();
         }
