ffd8940a536cb25f99e7b786d520ec29ee7e6ae4
unkedeuxke <kev.lefevre@free.fr>
2013-12-17 19:33:21 +0100

    Fix du bug de Music pour la suppression d'une Song

--- BitMusic/bitmusic/music/data/SongLibrary.java
+++ BitMusic/bitmusic/music/data/SongLibrary.java
@@ -5,127 +5,127 @@
  */
 
 package bitmusic.music.data;
- 
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.io.Serializable;
 
 
 public class SongLibrary implements Serializable{
-    
-   private ArrayList<Song> library; 
+
+   private ArrayList<Song> library;
    private static final long serialVersionUID = 300L;
 
    /**
-    * default constructor   
+    * default constructor
     */
    public SongLibrary(){
       library = new ArrayList<Song>();
     }
-   
+
    /**
     * constructor.
-    * 
-    * @param songs 
+    *
+    * @param songs
     */
-    
+
    public SongLibrary(ArrayList<Song> songs)
    {
     library = new ArrayList();
     library = songs;
    }
-   
+
    /**
     * Returns the list of songs.
-    * 
+    *
     * @return library
     */
    public ArrayList<Song> getlibrary()
    {
        return library;
    }
-   
+
    /**
     * Set the list of songs.
-    * 
-    * @param lib 
+    *
+    * @param lib
     */
    public void setlibrary (ArrayList<Song> lib)
    {
        library = lib;
    }
-   
+
    /**
     * Returns true if the song with the ID songID is in the list.
-    * 
+    *
     * @param songId
     * @return bool
     */
     public boolean islocal(String songId)
     {
-    
+
     Iterator<Song> it = library.iterator();
-    
+
     while (it.hasNext())
-    {  
+    {
         Song s = it.next();
         if (s.getSongId().equals(songId))
             return true;
- 
-        }  
+
+        }
     return false;
- } 
-   
+ }
+
    /**
     * returns the songID of a song.
     * @param songId
-    * @return 
+    * @return
     */
     public Song getSong(String songId)
     {
 	Iterator<Song> it = library.iterator();
-		
+
 	while (it.hasNext())
-	{  
+	{
             Song s = it.next();
             if (s.getSongId().equals(songId))
 		return s;
-	}  
+	}
 	return null;
     }
-   
+
    /**
     * Removes the song songId.
-    * 
-    * @param songId 
+    *
+    * @param songId
     */
    public void removeSong (String songId)
    {
-   Iterator<Song> it=library.iterator();
-   while(it.hasNext()) //while there is still a song in the library
-    {
-       Song song=it.next();
-       if(song.getSongId().equals(songId))
-       library.remove(song); //removes the song with the id songId from the library
-   
+        Iterator<Song> it=library.iterator();
+
+        int indexToRemove = -1;
+        for(int i = 0; i < library.size(); i++) {
+            if(library.get(i).getSongId().equals(songId))
+                indexToRemove = i;
+        }
+        library.remove(indexToRemove);
    }
-   }
-   
+
    /**
     * Adds song to the song list.
-    * 
-    * @param song 
+    *
+    * @param song
     */
    public void addSong(Song song)
-   {       
-       this.library.add(song);       
+   {
+       this.library.add(song);
    }
-   
+
    /**
     * Adds the category name to each song og the list song.
-    * 
+    *
     * @param name
-    * @param rights 
+    * @param rights
     */
    public void addCategory (String name, Rights rights)
    {
@@ -136,11 +136,11 @@ public class SongLibrary implements Serializable{
            song.updateCategory(name, rights);
        }
    }
-   
+
    /**
     * Removes the category name to each song of the song list.
-    * 
-    * @param name 
+    *
+    * @param name
     */
    public void removeCategory (String name)
    {
@@ -149,23 +149,23 @@ public class SongLibrary implements Serializable{
     {
        Song song = it.next();
        song.deleteCategory(name);// put default parameters to category name
-   
+
     }
    }
 }
-   
-   
-   
-   
 
 
-   
-   
-   
-   
-   
-   
-   
-   
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
 
